a47dd9286f49236ff0fcb5828cab64097444061c
Cory Bethune <corybbethune@gmail.com>
2022-05-22 11:34:48 -0700

    Update show draft 1

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -3,4 +3,11 @@ class AdminApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
+  def update
+    pet = Application.find(params[:id]).pets.find(params[:pet_id])
+    pet.adoptable = false
+    binding.pry
+    redirect_to "/admin/applications/#{params[:id]}"
+  end
+
 end
