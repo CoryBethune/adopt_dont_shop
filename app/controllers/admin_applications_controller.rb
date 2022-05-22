4105beb79f4b9574f163439a78f565609cc6164c
Cory Bethune <corybbethune@gmail.com>
2022-05-22 12:44:39 -0700

    Refactor

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -4,10 +4,10 @@ class AdminApplicationsController < ApplicationController
   end
 
   def update
+    pet = Application.find(params[:id]).pets.find(params[:pet_id])
     if params[:rejected].present?
       pet.application_pet.update(params[:rejected])
     else
-      pet = Application.find(params[:id]).pets.find(params[:pet_id])
       pet.update(adoptable: false)
     end
     redirect_to "/admin/applications/#{params[:id]}"
